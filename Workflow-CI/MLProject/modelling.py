6a40995dbec79bf98c95a27b02ce7b746091e0d8
Khoerunisa Salma Rahmadhani <115153134+NisaSalma@users.noreply.github.com>
2025-12-26 20:32:23 +0700

    Add files via upload

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -1,17 +1,15 @@
 import pandas as pd
-import os
-import mlflow
-import mlflow.sklearn
-
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
+import mlflow
+import mlflow.sklearn
+import os
 
 mlflow.set_tracking_uri("https://dagshub.com/NisaSalma/Membangun_Model.mlflow")
-mlflow.set_experiment("Ames Housing - Skilled")
+mlflow.set_experiment("Ames Housing - Basic Model")
+
+mlflow.sklearn.autolog(log_models=True)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "ames_preprocessing", "ames_preprocessed.csv")
@@ -25,41 +23,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("model", RandomForestRegressor(random_state=42))
-])
-
-param_grid = {
-    "model__n_estimators": [100, 200],
-    "model__max_depth": [10, 20, None]
-}
-
-grid_search = GridSearchCV(
-    pipeline,
-    param_grid,
-    cv=3,
-    scoring="r2",
-    n_jobs=-1
-)
-
 with mlflow.start_run():
 
-    grid_search.fit(X_train, y_train)
+    model = RandomForestRegressor(random_state=42)
+    model.fit(X_train, y_train)
 
-    best_model = grid_search.best_estimator_
-    y_pred = best_model.predict(X_test)
+    y_pred = model.predict(X_test)
 
     mse = mean_squared_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)
 
-    mlflow.log_params(grid_search.best_params_)
-
-    mlflow.log_metric("mse", mse)
-    mlflow.log_metric("r2", r2)
-
-    mlflow.sklearn.log_model(best_model, "model")
-
-    print("Best Params:", grid_search.best_params_)
     print("MSE:", mse)
     print("R2:", r2)
